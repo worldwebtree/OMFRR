ca876b0a04f55f7055261f08b059a661a224991b
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2022-11-15 22:23:28 +0500

    Created databse migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('gender', ['male', 'female', 'custom']);
+            $table->string('avatar');
+            $table->enum('role', ['admin', 'user']);
+
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2022_11_15_171449_create_ratting_keywords_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratting_keywords', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('keyword_name');
+            $table->integer('keyword_ratting');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratting_keywords');
+    }
+};
