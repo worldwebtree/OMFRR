c6169c5bfd2b28306840c608aca4d3441656e02a
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2022-11-16 17:29:10 +0500

    Creating roles and privliges

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard.Admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/**
+ * Administrator Routes
+ */
+Route::prefix('admin')->group( function() {
+
+    Route::get('/dashboard', function () {
+
+        return view('dashboard.Admin.dashboard');
+
+    })->middleware(['auth', 'isAdmin', 'verified'])
+    ->name('admin.dashboard');
+
+});
+
+/**
+ * User Routes
+ */
+Route::prefix('user')->group( function() {
+
+    Route::get('/dashboard', function () {
+
+        return view('dashboard.Customer.dashboard');
+
+    })->middleware(['auth', 'isUser', 'verified'])
+    ->name('user.dashboard');
+
+});
 
 require __DIR__.'/auth.php';
