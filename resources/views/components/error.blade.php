6a6e2f6067cea5df54a5ebb1592a0bac75aa2bb6
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2022-11-21 15:50:59 +0500

    Fixed display error statment in error component

--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,13 +1,13 @@
-@if ($errors)
-    @foreach ($errors as $error)
-        <div class="alert alert-primary alert-dismissible fade show" role="alert">
-            <button type="button" class="close border-none outline-none" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                <span class="sr-only">Close</span>
-            </button>
-            <ul>
+@if ($errors->any())
+    <div class="alert alert-primary alert-dismissible fade show" role="alert">
+        <button type="button" class="close border-none outline-none" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            <span class="sr-only">Close</span>
+        </button>
+        <ul>
+            @foreach ($errors->all( ) as $error)
                 <li>{{ $error }}</li>
-            </ul>
-        </div>
-    @endforeach
+            @endforeach
+        </ul>
+    </div>
 @endif
