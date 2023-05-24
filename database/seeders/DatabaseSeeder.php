183e4807c305d07151aeaedf88f4e8a98b5aefa3
Behram-Khattak <behramkttk9@gmail.com>
2023-05-24 15:16:51 +0300

    Removed comments from Database Seeders file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(100)->create();
+        User::factory(50)->create();
 
         User::factory()->create([
             'name' => 'Admin',
@@ -30,11 +30,15 @@ class DatabaseSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
 
-
-        // $users->each(function($user) {
-
-        //     $user->users_feedback()->saveMany(UsersFeedback::factory(10)->make());
-
-        // });
+        User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'gender' => 'male',
+            'avatar' => null,
+            'role' => 'user',
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
